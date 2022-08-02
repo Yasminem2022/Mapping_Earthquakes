0008d7f4e35eb35650e2eddaa2bd5a24393727d3
Yasminem2022 <yasminem66@gmail.com>
2022-08-01 20:56:58 -0400

    Completing earthquake map

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -29,6 +29,8 @@ let map = L.map('mapid', {
   layers: [streets]
 })
 
+
+
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
